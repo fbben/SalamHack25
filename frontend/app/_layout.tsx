678a25f48a2867232ce5515edbd10331e6ce254e
Benzii <farestech6@gmail.com>
2025-03-15 02:39:14 +0100

    setup Expo router

---
+++ frontend/app/_layout.tsx
@@ -0,0 +1,5 @@
+import { Stack } from "expo-router";
+//everything in layout will be rendered before any other page under /app.
+export default function RootLayout() {
+    return (<Stack></Stack>);
+}
---
+++ frontend/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen(){
+
+    return (
+        <View>
+            <Text style={{fontSize: 40}}> Home Screen. </Text>
+        </View>
+    );
+}
