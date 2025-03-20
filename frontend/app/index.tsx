72632e7a55db77be1161b0c7b2b1e657a9196a25
diar adem <ma_diar@esi.dz>
2025-03-20 07:10:09 +0100

    Update index.tsx

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,34 +1,9 @@
-import React, { useState } from "react";
-import { useForm, Controller } from "react-hook-form";
-import { VStack } from "@/components/ui/vstack";
 import { Text } from "@/components/ui/text";
-import { Input, InputField } from "@/components/ui/input";
-import { Button, ButtonText } from "@/components/ui/button";
-import { HStack } from "@/components/ui/hstack";
-import { FontAwesome } from "@expo/vector-icons";
-import {
-  FormControl,
-  FormControlError,
-  FormControlErrorText,
-  FormControlErrorIcon,
-  FormControlLabel,
-  FormControlLabelText,
-  FormControlHelper,
-  FormControlHelperText,
-} from "@/components/ui/form-control";
-import { AlertCircleIcon } from "@/components/ui/icon";
 import { styles } from "@/styles";
 
-import { useMutation } from "@tanstack/react-query";
-import { login } from "@/api/auth";
-import { storeToken, checkStoredToken } from "@/utils/expoStorage";
 import { Pressable } from "react-native";
 
-import Toast from "react-native-toast-message";
 import { useRouter } from "expo-router";
-import { ScrollView } from "react-native";
-
-import { Link } from "expo-router";
 
 export default function LandingPage() {
   const router = useRouter();
